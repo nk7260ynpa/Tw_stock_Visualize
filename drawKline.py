6f7c9282f3ac3d4597b5212c1a799a41c8f8f0b1
Chen <nk7260ynpa@gmail.com>
2025-06-24 00:16:24 +0800

    Add drawKline.py for visualizing daily stock prices with candlestick charts

---
+++ drawKline.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sqlalchemy import text
+import mplfinance as mpf
+
+from routers import MySQLRouter
+
+HOST = "localhost:3306"
+USER = "root"
+PASSWORD = "stock"
+DBNAME = "TWSE"
+
+conn = MySQLRouter(HOST, USER, PASSWORD, DBNAME).mysql_conn
+query = """
+    SELECT * 
+    FROM DailyPrice
+    WHERE Date BETWEEN '2025-01-01' AND '2025-06-20' 
+    AND SecurityCode = '2330'
+"""
+results = conn.execute(text(query)).fetchall()
+conn.close()
+
+df = pd.DataFrame(results)
+
+df['Date'] = pd.to_datetime(df['Date'])
+columns_to_convert = ['TradeVolume', 'OpeningPrice', 'HighestPrice', 'LowestPrice', 'ClosingPrice']
+df[columns_to_convert] = df[columns_to_convert].astype(float)
+
+df.rename({"Date": "date",
+           "TradeVolume": "volume", 
+           "OpeningPrice": "open",
+           "HighestPrice": "high",
+           "LowestPrice": "low",
+           "ClosingPrice": "close"}, axis=1, inplace=True)
+
+df.set_index('date', inplace=True)
+
+plt.figure(figsize=(16, 9), dpi=100)
+plt.tight_layout()
+mpf.plot(df, type='candle', style='yahoo',
+         title='2330 Daily Price',
+         ylabel='Price (TWD)',
+         volume=True,
+         mav=(5, 10, 20),
+         figratio=(16, 9),
+         figscale=1.2,
+         savefig='2330_daily_price.png',
+         show_nontrading=True)
